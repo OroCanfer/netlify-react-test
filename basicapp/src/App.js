5dd0c3355268720cef55b4cf4df376b919a80d8d
robertodg <jaro_4452@hotmail.com>
2019-01-10 20:55:25 -0500

    Update App.js

--- basicapp/src/App.js
+++ basicapp/src/App.js
@@ -9,7 +9,7 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            <code>src/App.js</code> Edited!
+            <code>src/App.js</code> Edited!!!!!!!!!!!!!
           </p>
           <a
             className="App-link"
